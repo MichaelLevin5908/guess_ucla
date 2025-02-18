b22440696ea590594915c134606231d787f69987
michaelLevin5908 <michaeljustinlevin@gmail.com>
2025-02-18 13:50:08 -0800

    obselete test due to auth fix later

--- guessucla/src/App.test.tsx
+++ guessucla/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders Guess UCLA heading', () => {
-  render(<App />);
-  const headingElement = screen.getByText(/Guess UCLA/i);
-  expect(headingElement).toBeInTheDocument();
-});
+// test('renders Guess UCLA heading', () => {
+//   render(<App />);
+//   const headingElement = screen.getByText(/Guess UCLA/i);
+//   expect(headingElement).toBeInTheDocument();
+// });
